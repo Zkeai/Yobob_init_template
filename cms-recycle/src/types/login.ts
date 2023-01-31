07cb613c2f9d1d29531825574cc031d22ea1dcac
zkeai <zq961052905@outlook.com>
2023-02-01 01:13:53 +0800

    update hupi init

---
+++ cms-recycle/src/types/login.ts
@@ -0,0 +1,10 @@
+export interface IAccount {
+  userAccount: string
+  userPassword: string
+}
+
+export interface IRegAccount {
+  userAccount: string
+  userPassword: string
+  checkPassword: string
+}
